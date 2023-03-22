ab6f25ac6b33b5db7cc0cf6baa145bc48a7a7d16
Ministorm3 <4474921+Ministorm3@users.noreply.github.com>
2023-03-22 13:22:32 -0400

    Update Config.js

--- js/Config.js
+++ js/Config.js
@@ -1,5 +1,5 @@
 window.CONFIG = {
-  crawl: `Thank you for watching! We will be right back after this short weather break. This forcast is geerated once per hour.`,
+  crawl: `Thank you for watching! We will be right back after this short weather break. This forcast is generated once per hour.`,
   greeting: 'This is your weather',
   language: 'en-US', // Supported in TWC API
   countryCode: 'US', // Supported in TWC API (for postal key)
